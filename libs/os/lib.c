e6ddb749e537a8ccd1d9c1ee4605c9cbb56dcc7d
Ivan Pidhurskyi <ivanpidhurskyi1997@gmail.com>
2020-09-20 20:32:25 +0200

    Upgrade Dope. Add `Os.mkdtemp`.

--- libs/os/lib.c
+++ libs/os/lib.c
@@ -72,6 +72,19 @@ _realpath(void)
   eth_return(args, eth_str(buf));
 }
 
+static eth_t
+_mkdtemp(void)
+{
+  eth_args args = eth_start(1);
+  eth_t temp = eth_arg2(args, eth_string_type);
+  char buf[eth_str_len(temp)+1];
+  strcpy(buf, eth_str_cstr(temp));
+  if (not mkdtemp(buf))
+    eth_throw(args, eth_str(eth_errno_to_str(errno)));
+  else
+    eth_return(args, eth_str(buf));
+}
+
 int
 ether_module(eth_module *mod, eth_env *topenv)
 {
@@ -84,6 +97,8 @@ ether_module(eth_module *mod, eth_env *topenv)
   eth_define(mod, "__access", eth_create_proc(_access, 2, NULL, NULL));
   eth_define(mod, "__getcwd", eth_create_proc(_getcwd, 0, NULL, NULL));
   eth_define(mod, "__realpath", eth_create_proc(_realpath, 1, NULL, NULL));
+
+  eth_define(mod, "__mkdtemp", eth_create_proc(_mkdtemp, 1, NULL, NULL));
 
   int ret = 0;
   if (not eth_load_module_from_script2(topenv, NULL, mod, "./lib.eth", NULL, mod))
