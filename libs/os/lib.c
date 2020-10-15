1e9460b9420582881fc4259ad1accf77aa120c91
Ivan Pidhurskyi <ivanpidhurskyi1997@gmail.com>
2020-10-15 14:27:25 +0200

    Add Os.pipe & Os.pipe2.

--- libs/os/lib.c
+++ libs/os/lib.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include "ether/ether.h"
 
 #include <stdlib.h>
@@ -5,6 +6,7 @@
 #include <assert.h>
 #include <errno.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 static eth_t
 _chdir(void)
@@ -104,6 +106,46 @@ _fork(void)
   return pid < 0 ? eth_exn(eth_system_error(errno)) : eth_num(pid);
 }
 
+static eth_t
+_pipe(void)
+{
+  eth_use_variant(System_error);
+
+  int fildes[2];
+  if (pipe(fildes) < 0)
+  {
+    return eth_exn(System_error(eth_str(eth_errno_to_str(errno))));
+  }
+  else
+  {
+    eth_t rx = eth_open_fd(fildes[0], "r");
+    eth_t tx = eth_open_fd(fildes[1], "w");
+    return eth_tup2(rx, tx);
+  }
+}
+
+static eth_t
+_pipe2(void)
+{
+  eth_use_variant(System_error);
+
+  eth_args args = eth_start(1);
+  eth_t flags = eth_arg2(args, eth_number_type);
+
+  int fildes[2];
+  if (pipe2(fildes, eth_num_val(flags)) < 0)
+  {
+    eth_throw(args, System_error(eth_str(eth_errno_to_str(errno))));
+  }
+  else
+  {
+    eth_t rx = eth_open_fd(fildes[0], "r");
+    eth_t tx = eth_open_fd(fildes[1], "w");
+    eth_return(args, eth_tup2(rx, tx));
+  }
+}
+
+
 int
 ether_module(eth_module *mod, eth_env *topenv)
 {
@@ -123,6 +165,14 @@ ether_module(eth_module *mod, eth_env *topenv)
   eth_define(mod, "__mkdtemp", eth_proc(_mkdtemp, 1));
 
   eth_define(mod, "__fork", eth_proc(_fork));
+
+  eth_define(mod, "__pipe", eth_proc(_pipe));
+  eth_define(mod, "__pipe2", eth_proc(_pipe2, 1));
+
+  eth_define(mod, "o_cloexec", eth_num(O_CLOEXEC));
+  eth_define(mod, "o_direct", eth_num(O_DIRECT));
+  eth_define(mod, "o_nonblock", eth_num(O_NONBLOCK));
+
 
   if (not eth_add_module_script(mod, "./lib.eth", topenv))
     return -1;
