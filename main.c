da0dfe93bde2a90264f2a3255921da3704d1d2ad
Ivan Pidhurskyi <ivanpidhurskyi1997@gmail.com>
2020-11-05 01:19:25 +0100

    Add GPL.

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+/* Copyright (C) 2020  Ivan Pidhurskyi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "ether/ether.h"
 #include "codeine/vec.h"
 
@@ -15,6 +30,9 @@ ETH_MODULE("ether:main")
 
 static void __attribute__((noreturn))
 help_and_exit(char *argv0);
+
+static void
+print_GPL(void);
 
 static void
 print_version(FILE *out);
@@ -228,8 +246,8 @@ main(int argc, char **argv)
     else
       system("touch ~/.ether_history");
 
-    printf("Ether REPL\n");
-    print_version(stdout);
+    printf("ether %s\n", eth_get_version());
+    print_GPL();
     printf("\n");
     repl_help(stdout);
     printf("\n");
@@ -492,6 +510,25 @@ help_and_exit(char *argv0)
   puts("                                <head>,<tail> - specify both head- and tail-length;");
   puts("                                <head>        - specify only head-length while tail-length is set to 1.");
   exit(EXIT_SUCCESS);
+}
+
+static void
+print_GPL(void)
+{
+  printf("Copyright (C) 2020  Ivan Pidhurskyi\n");
+  puts("");
+  printf("This program is free software: you can redistribute it and/or modify\n");
+  printf("it under the terms of the GNU General Public License as published by\n");
+  printf("the Free Software Foundation, either version 3 of the License, or\n");
+  printf("(at your option) any later version.\n");
+  puts("");
+  printf("This program is distributed in the hope that it will be useful,\n");
+  printf("but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+  printf("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n");
+  printf("GNU General Public License for more details.\n");
+  puts("");
+  printf("You should have received a copy of the GNU General Public License\n");
+  printf("along with this program.  If not, see <https://www.gnu.org/licenses/>.\n");
 }
 
 static void
