c395731886702f93a099a5e3ad05bf3d850a2040
Ivan Pidhurskyi <ivan.pidhurskyi@cern.ch>
2022-04-10 14:55:15 +0200

    Dont show exceptions in dope, update LD_LIBRARY_PATH in env.sh, update samples/basics.eth.

--- env.sh
+++ env.sh
@@ -3,4 +3,5 @@ p=`realpath ${1:?installation path not specified}`
 export PATH=$p/bin:$PATH
 export ETHER_ROOT=$p
 export ETHER_PATH=$p/lib/ether
+export LD_LIBRARY_PATH=$p/lib${LD_LIBRARY_PATH+:${LD_LIBRARY_PATH}}
 export PKG_CONFIG_PATH=$p/lib/pkgconfig${PKG_CONFIG_PATH+:${PKG_CONFIG_PATH}}
