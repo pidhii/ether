bd21643f84ec9fbcd6898c3fa1dc2cb4afec4699
Ivan Pidhurskyi <ivan.pidhurskyi@cern.ch>
2023-03-01 11:34:50 +0100

    [os] Add functions `waitpid` and `fileno`.

--- libs/os/lib.c
+++ libs/os/lib.c
@@ -1,15 +1,15 @@
 /* Copyright (C) 2020  Ivan Pidhurskyi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -22,6 +22,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 static eth_t
 _chdir(void)
@@ -143,6 +144,21 @@ _fork(void)
 }
 
 static eth_t
+_waitpid(void)
+{
+  eth_args args = eth_start(2);
+  eth_t pid = eth_arg2(args, eth_number_type);
+  eth_t options = eth_arg2(args, eth_number_type);
+  int wstatus = 0;
+
+  int ret = waitpid(eth_num_val(pid), &wstatus, (int)eth_num_val(options));
+  int err = errno;
+  if (ret < 0)
+    eth_throw(args, eth_system_error(err));
+  eth_return(args, eth_tup2(eth_num(ret), eth_num(wstatus)));
+}
+
+static eth_t
 _pipe(void)
 {
   eth_use_variant(system_error);
@@ -181,6 +197,20 @@ _pipe2(void)
   }
 }
 
+static eth_t
+_fileno(void)
+{
+  eth_args args = eth_start(1);
+  eth_t file = eth_arg2(args, eth_file_type);
+
+  int fd = fileno(eth_get_file_stream(file));
+  int err = errno;
+  if (fd < 0)
+    eth_throw(args, eth_system_error(err));
+  else
+    eth_return(args, eth_num(fd));
+}
+
 
 int
 ether_module(eth_module *mod, eth_root *topenv)
@@ -206,9 +236,12 @@ ether_module(eth_module *mod, eth_root *topenv)
   eth_define(detail, "__mkdtemp", eth_proc(_mkdtemp, 1));
 
   eth_define(detail, "__fork", eth_proc(_fork));
+  eth_define(detail, "__waitpid", eth_proc(_waitpid, 2));
 
   eth_define(detail, "__pipe", eth_proc(_pipe));
   eth_define(detail, "__pipe2", eth_proc(_pipe2, 1));
+
+  eth_define(detail, "__fileno", eth_proc(_fileno, 1));
 
   eth_define(detail, "__o_cloexec", eth_num(O_CLOEXEC));
   eth_define(detail, "__o_direct", eth_num(O_DIRECT));
